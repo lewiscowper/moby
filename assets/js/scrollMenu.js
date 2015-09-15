114404dcf12753854e59c68d5b7ff31c25e0799f
Lewis Cowper <lewis.cowper@googlemail.com>
2015-09-15 17:50:43 +0200

    update height that we trigger the class addition

--- assets/js/scrollMenu.js
+++ assets/js/scrollMenu.js
@@ -1,12 +1,14 @@
 document.addEventListener("DOMContentLoaded", function(event) {
   var container = document.body.children[0];
-  
+
   var checkScroll = function () {
     var scrollPosition = container.scrollTop;
     var headerHeight = document.getElementsByTagName('header')[0].clientHeight;
     var menuButton = document.getElementById('open-button');
+    var menuButtonHeight = menuButton.clientHeight;
+    var targetHeight = headerHeight - menuButtonHeight;
 
-    if (scrollPosition > headerHeight) {
+    if (scrollPosition > targetHeight) {
       menuButton.classList.add('is-scrolled');
     } else {
       menuButton.classList.remove('is-scrolled');
